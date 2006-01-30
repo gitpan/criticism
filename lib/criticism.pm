9c9a9e509e4f70d9a625fa77ecefad2e0547a0a3
Jeffrey Ryan Thalhammer <jeff@thaljef.org>
2006-01-30 04:27:14 +0000

    Import of THALJEF/criticism-0.03 from CPAN.
    gitpan-cpan-distribution: criticism
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         THALJEF/criticism/criticism-0.03.tar.gz
    gitpan-cpan-author:       THALJEF
    gitpan-cpan-maturity:     released

--- lib/criticism.pm
+++ lib/criticism.pm
@@ -1,6 +1,6 @@
 #######################################################################
 #      $URL: http://perlcritic.tigris.org/svn/perlcritic/trunk/criticism/lib/criticism.pm $
-#     $Date: 2006-01-14 23:04:45 -0800 (Sat, 14 Jan 2006) $
+#     $Date: 2006-01-21 22:21:36 -0800 (Sat, 21 Jan 2006) $
 #   $Author: thaljef $
 # $Revision: 203 $
 ########################################################################
@@ -9,12 +9,11 @@ package criticism;
 
 use strict;
 use warnings;
-use Perl::Critic;
-use Perl::Critic::Violation;
+use English qw(-no_match_vars);
 use Carp;
 
-our $VERSION = 0.02;
-$VERSION = eval "$VERSION";  ## no critic
+our $VERSION = 0.03;
+$VERSION = eval $VERSION;  ## no critic
 
 #-----------------------------------------------------------------------------
 
@@ -26,28 +25,44 @@ my %severity_of = (
     brutal => 1,
 );
 
+my $default_mood = 'gentle';
+
 #-----------------------------------------------------------------------------
 
 sub import {
-    my ($pkg, $mood) = @_;
 
-    if (defined $mood && ! exists $severity_of{$mood} ){
-        my @moods = sort { $severity_of{$b} <=> $severity_of{$a} }
-            keys %severity_of;
+    my ($pkg, $mood) = @_;
+    $mood ||= $default_mood;
+
+    if (! exists $severity_of{$mood} ){
+        my @moods = keys %severity_of;
+        @moods = sort { $severity_of{$b} <=> $severity_of{$a} } @moods;
         croak qq{'$mood' criticsm not supported.  Choose from: @moods};
     }
 
     my $file = (caller)[1];
     return 1 if ! -f $file;
+    my @violations = ();
 
-    my $sev          = $severity_of{$mood || 'gentle'};
-    my $critic       = Perl::Critic->new( -severity => $sev );
-    my @violations   = $critic->critique($file);
+    eval {
+        require Perl::Critic;
+        require Perl::Critic::Violation;
+	my $sev     = $severity_of{$mood};
+        my $critic  = Perl::Critic->new( -severity => $sev );
+        @violations = $critic->critique($file);
 
-    if (@violations) {
-        local $Perl::Critic::Violation::FORMAT
-            = "$file: %m at line %l, column %c. %e.\n";
-        warn @violations;
+        if (@violations) {
+            ## no critic
+            no warnings 'once';
+            my $format = "$file: %m at line %l, column %c. %e.\n";
+            local $Perl::Critic::Violation::FORMAT = $format;
+            warn @violations;
+        }
+    };
+
+    if ( $EVAL_ERROR && ($ENV{DEBUG} || $PERLDB) ) {
+        carp qq{'criticism' failed to load: $EVAL_ERROR};
+        return;
     }
 
     return @violations ? 0 : 1;
@@ -64,7 +79,7 @@ __END__
 
 =head1 NAME
 
-criticism - Perl pragma to enforce coding standards
+criticism - Perl pragma to enforce coding standards and best-practices
 
 =head1 SYNOPSIS
 
@@ -78,46 +93,49 @@ criticism - Perl pragma to enforce coding standards
 
 =head1 DESCRIPTION
 
-This pragma runs your file through L<Perl::Critic> before every
-execution.  In practice, this isn't always feasible because it adds a
-great deal of overhead at start-up.  Unless you really don't mind the
-wait, you're probably better off using the L<perlcritic> command-line
-or L<Test::Perl::Critic>.
+This pragma enforces coding standards and promotes best-practices by
+running your file through L<Perl::Critic> before every execution.  In
+a production system, this usually isn't feasible because it adds a lot
+of overhead at start-up.  If you have a separate development
+environment, you can effectively bypass the C<criticism> pragma by not
+installing L<Perl::Critic> in the production environment.  If
+L<Perl::Critic> can't be loaded, then C<criticism> just fails
+silently.
 
-For scripts, you can manually load the C<criticism> pragma at the
-command-line like this:
-
-  perl -Mcriticism=cruel your_script.pl
-
-Or if you only want to run L<Perl::Critic> during development and you
-have not installed Perl::Critic in your production environment, try
-this:
-
-  eval {
-      require criticism;
-      criticism->import( 'cruel' );
-  };
+Alternatively, the C<perlcritic> command-line (which is distributed
+with L<Perl::Critic>) can be used to analyze your files on-demand and
+has some additional configuration features.  And L<Test::Perl::Critic>
+provides a nice interface for analyzing files during the build
+process.
 
 =head1 CONFIGURATION
 
 The import argument is a named equivalent to the numeric severity
-levels in Perl::Critic.  For example, C<use criticism 'gentle';>
+levels in L<Perl::Critic>.  For example, C<use criticism 'gentle';>
 reports only the most dangerous violations.  On the other hand, C<use
 criticism 'brutal';> reports B<every> violation.  If the import
 argument is not defined, it defaults to C<'gentle'>.
 
-The C<criticsm> pragma will obey whatever configurations you have set
+The C<criticism> pragma will obey whatever configurations you have set
 in your F<.perlcriticrc> file.  See L<Perl::Critic/"CONFIGURATION">
 for more details.
 
+=head1 DIAGNOSTICS
+
+Usually, the C<criticism> pragma fails silently.  But if you set the
+C<DEBUG> environment variable to a true value or run your program
+under the Perl debugger, you will get a warning when C<criticism>
+fails to load L<Perl::Critic>.
+
 =head1 NOTES
 
-The C<criticism> pragma will apply to the whole file, so it is not
-affected by scope boundaries and C<use>-ing it mutliple times will
-just cause it to reprocess the same file.  There isn't a reciprocal
-C<no criticism> pragma.  However, L<Perl::Critic> does support a
-pseudo-pragma that directs it to overlook certain lines or blocks of
-code.  See L<Perl::Critic/"BENDING THE RULES"> for more details.
+The C<criticism> pragma applies to the entire file, so it is not
+affected by scope or package boundaries and C<use>-ing it multiple
+times will just cause it to repeatedly process the same file.  There
+isn't a reciprocal C<no criticism> pragma.  However, L<Perl::Critic>
+does support a pseudo-pragma that directs it to overlook certain lines
+or blocks of code.  See L<Perl::Critic/"BENDING THE RULES"> for more
+details.
 
 =head1 AUTHOR
 
